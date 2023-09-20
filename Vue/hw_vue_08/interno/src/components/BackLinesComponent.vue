988a96c21bc4e556dacf3984224913713bce635a
SergeyXP <s.pusthotin@gmail.com>
2023-09-20 23:51:26 +0400

    hw_layout_08

---
+++ Vue/hw_vue_08/interno/src/components/BackLinesComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="back-lines">
+    <div class="back-line" :style="{ left: `calc((100% - 1200px) / 2 + ${x}px)` }" v-for="x in lineList"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackLines',
+
+  data() {
+    return {
+      lineList: [0, 400, 800, 1200],
+    };
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Vue/hw_vue_08/interno/src/components/BodyContentComponent.vue
@@ -0,0 +1,25 @@
+<script>
+import BackLines from './BackLinesComponent.vue';
+
+export default {
+  name: 'BodyContent',
+
+  data() {
+    return {};
+  },
+  components: { BackLines },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="body-content">
+    <BackLines></BackLines>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
