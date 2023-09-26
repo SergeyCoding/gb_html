97c011a08440229a7622c5e370af90d41c640e5f
SergeyXP <s.pusthotin@gmail.com>
2023-09-26 18:47:24 +0400

    hw_vue_10

--- Vue/hw_vue_10/src/App.vue
+++ Vue/hw_vue_10/src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homework 10</h1>
+    <p>
+      Вы разрабатываете приложение для интернет-магазина и у вас есть компонент Vue под названием "ProductDetails".
+      Компонент отображает детали о конкретном продукте, включая его название, цену и статус доступности. Внутри
+      компонента "ProductDetails" создайте свойство "product" с объектом, представляющим информацию о продукте. Объект
+      должен иметь свойства "name" (название продукта), "price" (цена продукта) и "available" (флаг, указывающий на
+      доступность продукта). Используя вычисляемое свойство, назовите его "formattedPrice", которое будет возвращать
+      форматированную цену продукта со знаком валюты. Например, если цена равна 99.99, вычисляемое свойство должно
+      вернуть строку "$99.99". В компоненте "ProductDetails" отобразите название продукта, его форматированную цену и
+      статус доступности. Если продукт доступен, отобразите текст "Available", в противном случае - "Out of stock".
+    </p>
+
+    <div class="product-details">
+      <ProductDetails :currency="currency" />
+    </div>
+
+    <div>
+      <select v-model="currency">
+        <option value="RUB">рубли</option>
+        <option value="USD">доллары</option>
+        <option value="EUR">евро</option>
+      </select>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from './components/ProductDetails.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ProductDetails },
+  data() {
+    return {
+      currency: 'RUB',
+    };
+  },
+};
 </script>
 
 <style>
@@ -24,5 +48,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+p {
+  width: 80%;
+  margin: 0 auto;
+  text-align: justify;
+  line-height: 140%;
+}
+.product-details {
+  margin: 30px auto;
+  width: 400px;
 }
 </style>
---
+++ Vue/hw_vue_10/src/components/ProductDetails.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <p class="caption">Product Details</p>
+    <p class="name">Название: "{{ product.name }}"</p>
+    <p class="price">Цена: <span v-html="formattedPrice"></span></p>
+    <p class="available" v-if="product.available">Available</p>
+    <p class="out-of-stock" v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+
+  props: {
+    currency: String,
+  },
+
+  data() {
+    return {
+      product: { name: 'название продукта', price: 1234.56, available: true },
+    };
+  },
+
+  computed: {
+    formattedPrice() {
+      switch (this.currency) {
+        case 'EUR':
+          return '&euro;' + this.product.price;
+        case 'RUB':
+          return this.product.price + ' р.';
+        case 'USD':
+          return '$' + this.product.price;
+
+        default:
+          return this.product.price + ' руб. ';
+      }
+    },
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  min-width: 100px;
+  min-height: 100px;
+  border: 1px solid salmon;
+  background-color: beige;
+  border-radius: 10px;
+  padding: 15px;
+}
+.caption {
+  text-align: center;
+  margin-bottom: 15px;
+}
+.available {
+  color: green;
+}
+.out-of-stock {
+  color: red;
+}
+
+p {
+  margin-top: 10px;
+}
+</style>
