1a415910e720c64f0f33139f137f14ca9a15014a
SergeyXP <SergeyXP@gmail.com>
2023-10-02 08:12:04 +0400

    vue-hw12

---
+++ Vue/hw_vue_12/interno/src/components/BlogDetailsPage/BlogDetailsPageComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="blog-details-page__banner"></section>
+    <!--  -->
+    <main class="blog-details-page">
+      <BlogDetails></BlogDetails>
+    </main>
+  </div>
+</template>
+
+<script>
+import BlogDetails from './BlogDetailsComponent.vue';
+export default {
+  name: 'BlogDetailsPageComponent',
+
+  data() {
+    return {};
+  },
+
+  components: { BlogDetails },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
