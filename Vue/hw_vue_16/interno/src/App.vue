ae180fc305d9740425eedbef8bfbf744ef8da9ab
SergeyXP <s.pusthotin@gmail.com>
2023-10-07 13:47:47 +0400

    vue-last

--- Vue/hw_vue_16/interno/src/App.vue
+++ Vue/hw_vue_16/interno/src/App.vue
@@ -3,27 +3,32 @@
     <BodyContent>
       <Header></Header>
       <router-view></router-view>
-      <Footer></Footer>
+      <Footer v-if="isRightPage"></Footer>
     </BodyContent>
   </div>
 </template>
 
 <script>
-import BodyContent from "./components/BodyContentComponent.vue";
-import Header from "./components/BodyContent/HeaderComponent.vue";
-import Footer from "./components/BodyContent/FooterComponent.vue";
+import BodyContent from './components/BodyContentComponent.vue';
+import Header from './components/BodyContent/HeaderComponent.vue';
+import Footer from './components/BodyContent/FooterComponent.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     BodyContent,
     Header,
     Footer,
   },
+  computed: {
+    isRightPage() {
+      return this.$route.name !== 'Page404';
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-// @import "./scss/test";
-@import "./scss/style";
+// @import './scss/test';
+@import './scss/style';
 </style>
